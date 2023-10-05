0e615c3b568566d86c75dee68fe432f1e17f94c3
KlimVladMaks <vkm.blog@yandex.ru>
2023-10-06 00:16:39 +0300

    Добавление дополнительных таблиц

--- session-database.sql
+++ session-database.sql
@@ -9,3 +9,28 @@ CREATE TABLE teachers (
     patronymic TEXT,
     position TEXT NOT NULL
 );
+
+-- Таблица со студентами
+CREATE TABLE students (
+    record_book INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    patronymic TEXT,
+    course INTEGER
+);
+
+-- Таблица с аудиториями
+CREATE TABLE auditoriums (
+    id INTEGER PRIMARY KEY,
+    site_name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    number INTEGER NOT NULL
+);
+
+-- Таблица с учебными группами
+CREATE TABLE study_group (
+    code INTEGER PRIMARY KEY,
+    number TEXT NOT NULL,
+    from DATE NOT NULL,
+    to DATA
+);
