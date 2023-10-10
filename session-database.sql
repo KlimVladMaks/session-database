f675696451541dee0cd9b82e0feea47f66f2f2da
KlimVladMaks <vkm.blog@yandex.ru>
2023-10-10 15:20:27 +0300

    Commit

--- session-database.sql
+++ session-database.sql
@@ -1,16 +1,17 @@
--- DONE
 -- Создание базы данных
 CREATE DATABASE session_database;
 
 -- ТАБЛИЦЫ БЕЗ ВНЕШНИХ КЛЮЧЕЙ ------------------------
 
+-- ADDED
 -- Таблица со стипендиями
 CREATE TABLE scholarships (
     s_id INTEGER PRIMARY KEY,
     s_type VARCHAR(100) NOT NULL,
-    s_name VARCHAR(100) NOT NULL,
+    s_name VARCHAR(100) NOT NULL
 );
 
+-- ADDED
 -- Таблица со студентами
 CREATE TABLE students (
     s_record_book INTEGER PRIMARY KEY,
@@ -20,12 +21,14 @@ CREATE TABLE students (
     s_course INTEGER
 );
 
+-- ADDED
 -- Таблица с площадками института
 CREATE TABLE sites (
     s_name VARCHAR(100) PRIMARY KEY,
     s_address VARCHAR(100) NOT NULL
 );
 
+-- ADDED
 -- Таблица с преподавателями
 CREATE TABLE teachers (
     t_id INTEGER PRIMARY KEY,
@@ -35,6 +38,7 @@ CREATE TABLE teachers (
     t_position VARCHAR(100) NOT NULL
 );
 
+-- ADDED
 -- Таблица с дисциплинами
 CREATE TABLE disciplines (
     d_code INTEGER PRIMARY KEY,
@@ -42,9 +46,10 @@ CREATE TABLE disciplines (
     d_hours INTEGER NOT NULL,
     d_lecture_hours INTEGER NOT NULL,
     d_practical_hours INTEGER NOT NULL,
-    d_practical_hours INTEGER NOT NULL
+    d_laboratory_hours INTEGER NOT NULL
 );
 
+-- ADDED
 -- Таблица с направлениями
 CREATE TABLE directions (
     d_code INTEGER PRIMARY KEY,
@@ -52,6 +57,7 @@ CREATE TABLE directions (
     d_qualification VARCHAR(100) NOT NULL
 );
 
+-- ADDED
 -- Таблица с подразделениями
 CREATE TABLE divisions (
     d_code INTEGER PRIMARY KEY,
@@ -60,6 +66,7 @@ CREATE TABLE divisions (
 
 -- ТАБЛИЦЫ С ВНЕШНИМИ КЛЮЧАМИ ------------------------
 
+-- ADDED
 -- Таблица с аудиториями
 CREATE TABLE auditoriums (
     a_id INTEGER PRIMARY KEY,
@@ -69,55 +76,63 @@ CREATE TABLE auditoriums (
     FOREIGN KEY (a_site_name) REFERENCES sites (s_name)
 );
 
+-- ADDED
 -- Таблица с образовательными программами
 CREATE TABLE educational_programmes (
     ep_code INTEGER PRIMARY KEY,
     ep_name VARCHAR(100) NOT NULL,
     ep_division_code INTEGER NOT NULL,
     ep_direction_code INTEGER NOT NULL,
-    ep_format VARCHAR(50) NOT NULL
+    ep_format VARCHAR(50) NOT NULL,
     FOREIGN KEY (ep_division_code) REFERENCES divisions (d_code),
     FOREIGN KEY (ep_direction_code) REFERENCES directions (d_code)
 );
 
+-- ADDED
 -- Таблица с учебными планами
 CREATE TABLE curricula (
     c_code INTEGER PRIMARY KEY,
     c_educational_program_code INTEGER NOT NULL,
-    c_admission_year DATA NOT NULL,
+    c_admission_year DATE NOT NULL,
     FOREIGN KEY (c_educational_program_code) REFERENCES educational_programmes (ep_code)
 );
 
+-- ADDED
 -- Таблица с учебными группами
 CREATE TABLE study_groups (
     sg_code INTEGER PRIMARY KEY,
     sg_number VARCHAR(50) NOT NULL,
     sg_curriculum_code INTEGER NOT NULL,
     sg_from DATE NOT NULL,
-    sg_to DATA,
+    sg_to DATE,
     FOREIGN KEY (sg_curriculum_code) REFERENCES curricula (c_code)
 );
 
+-- ADDED
 -- Таблица с данными о получении стипендий
 CREATE TABLE scholarship_receipts (
-    sr_scholarship_id INTEGER PRIMARY KEY,
-    sr_student_record_book INTEGER PRIMARY KEY,
-    sr_date DATA PRIMARY KEY,
+    sr_scholarship_id INTEGER,
+    sr_student_record_book INTEGER,
+    sr_date DATE,
     sr_sum INTEGER NOT NULL,
+    PRIMARY KEY (sr_scholarship_id, sr_student_record_book, sr_date),
     FOREIGN KEY (sr_scholarship_id) REFERENCES scholarships (s_id),
     FOREIGN KEY (sr_student_record_book) REFERENCES students (s_record_book)
 );
 
+-- ADDED
 -- Таблица со студентами в учебных группах
 CREATE TABLE students_in_study_groups (
-    sisg_student_record_book INTEGER PRIMARY KEY,
-    sisg_study_group_code INTEGER PRIMARY KEY,
+    sisg_student_record_book INTEGER,
+    sisg_study_group_code INTEGER,
     sisg_from DATE NOT NULL,
-    sisg_to DATA,
+    sisg_to DATE,
+    PRIMARY KEY (sisg_student_record_book, sisg_study_group_code),
     FOREIGN KEY (sisg_student_record_book) REFERENCES students (s_record_book),
-    FOREIGN KEY (sisg_study_group_code) REFERENCES study_group (sg_code)
-)
+    FOREIGN KEY (sisg_study_group_code) REFERENCES study_groups (sg_code)
+);
 
+-- ADDED
 -- Таблица с дисциплинами в учебных планах
 CREATE TABLE disciplines_in_curricula (
     dic_code INTEGER PRIMARY KEY,
@@ -126,38 +141,45 @@ CREATE TABLE disciplines_in_curricula (
     dic_semester INTEGER NOT NULL,
     FOREIGN KEY (dic_discipline_code) REFERENCES disciplines (d_code),
     FOREIGN KEY (dic_curriculum_code) REFERENCES curricula (c_code)
-)
+);
 
+-- ADDED
 -- Таблица с подразделениями на площадках
 CREATE TABLE divisions_on_sites (
-    dos_site_name VARCHAR(100) PRIMARY KEY,
-    dos_division_code INTEGER PRIMARY KEY,
+    dos_site_name VARCHAR(100),
+    dos_division_code INTEGER,
+    PRIMARY KEY (dos_site_name, dos_division_code),
     FOREIGN KEY (dos_site_name) REFERENCES sites (s_name),
     FOREIGN KEY (dos_division_code) REFERENCES divisions (d_code)
-)
+);
 
+-- ADDED
 -- Таблица с аттестациями
 CREATE TABLE attestations (
-    a_discipline_in_curricula_code INTEGER PRIMARY KEY,
-    a_date DATA PRIMARY KEY,
+    a_discipline_in_curricula_code INTEGER,
+    a_date DATE,
     a_student_record_book INTEGER NOT NULL,
     a_attempt INTEGER NOT NULL,
     a_semester INTEGER NOT NULL,
     a_grade VARCHAR(10) NOT NULL,
     a_teacher_id INTEGER NOT NULL,
+    PRIMARY KEY (a_discipline_in_curricula_code, a_date),
     FOREIGN KEY (a_discipline_in_curricula_code) REFERENCES disciplines_in_curricula (dic_code),
     FOREIGN KEY (a_student_record_book) REFERENCES students (s_record_book),
     FOREIGN KEY (a_teacher_id) REFERENCES teachers (t_id)
-)
+);
 
+-- ADDED
+-- Таблица с расписанием сессии
 CREATE TABLE session_schedule (
-    ss_study_group_code INTEGER PRIMARY KEY,
-    ss_discipline_in_curriculum_code INTEGER PRIMARY KEY,
-    ss_date DATA PRIMARY KEY,
+    ss_study_group_code INTEGER,
+    ss_discipline_in_curriculum_code INTEGER,
+    ss_date DATE,
     ss_auditorium_id INTEGER,
     ss_teacher_id INTEGER NOT NULL,
+    PRIMARY KEY (ss_study_group_code, ss_discipline_in_curriculum_code, ss_date),
     FOREIGN KEY (ss_study_group_code) REFERENCES study_groups (sg_code),
     FOREIGN KEY (ss_discipline_in_curriculum_code) REFERENCES disciplines_in_curricula (dic_code),
     FOREIGN KEY (ss_auditorium_id) REFERENCES auditoriums (a_id),
     FOREIGN KEY (ss_teacher_id) REFERENCES teachers (t_id)
-)
+);
